2bc2f09fd33a16356d9d7ae067dbd1efa497923d
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-12-17 20:00:31 +0530

    added inngest function

---
+++ app/api/inngest/functions.js
@@ -0,0 +1,51 @@
+import { inngest } from "./client";
+
+
+
+//ingest function to add a user in database 
+export const syncUserCreation =inngest.createFunction(
+    {id:'sync-user-create'},
+    {event:'clerk/user.created'},
+    async ({ event}) => {
+        const {data} =event
+        await prisma.user.create({
+            data:{
+                id:data.id,
+                email:data.email_addresses[0].email_address,
+                name:`${data.first_name} ${data.last_name}`,
+                image:data.image_url,
+            }
+        })
+    }
+)
+
+// Inngest function to update user data in database
+
+export const syncUserUpdation =inngest.createFunction(
+    {id:'sync-user-update'},
+    {event:'clerk/user.updated'},
+    async ({event}) => {
+        const {data} =event
+        await prisma.user.update({
+            where:{id:data.id,},
+            data:{
+                email:data.email_addresses[0].email_address,
+                name:`${data.first_name} ${data.last_name}`,
+                image:data.image_url,
+            }
+        })
+    }
+)
+
+//Ingets function to dleete user from databse 
+
+export const syncUserDeletion =inngest.createFunction(
+    {id:'sync-user-delete'},
+    {event:'clerk/user.deleted'},
+    async ({event}) => {
+        const {data} =event
+        await prisma.user.delete({
+            where:{id:data.id,}
+        })
+    }
+)
