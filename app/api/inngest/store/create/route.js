03d0843c28044f21849cac37bc1bec180106933c
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-12-18 09:27:22 +0530

    added some imagekit functions

--- app/api/inngest/store/create/route.js
+++ app/api/inngest/store/create/route.js
@@ -1,3 +1,4 @@
+import imagekit from "@/configs/imageKit"
 import { getAuth } from "@clerk/nextjs/server"
 import { NextResponse } from "next/server"
 
@@ -42,7 +43,76 @@ export async function POST(request){
        if(isUsernameTaken){
         return NextResponse.json({error:"username already taken"}, {status:400})
        }
-    } catch(error) {
+//image upload to imagekit
+    const buffer=Buffer.from(await image.arrayBuffer());
+    const response =await imagekit.upload({
+        file:buffer,
+        fileName:image.name,
+        folder:"logos"
+    })
 
+    const optimizedImage=imagekit.url({
+        path:response.filePath,
+        transformation:[
+            {quality:'auto'},
+            {format:'webp'},
+            {width:'512'} 
+        ]
+    })
+
+    const newStore =await prisma.store.create({
+        data:{
+            userId,
+            name,
+            description,
+            username:username.toLowerCase(),
+            email,
+            contact,
+            address,
+            logo:optimizedImage
+        }
+    })
+
+    //link store to user
+    await prisma.user.update({
+        where:{id:userId},
+        data:{store:{connect:{id:newStore.id}}}
+    })
+
+    return NextResponse.json({message:"applied,waithing for approval"})
+
+
+    } catch(error) {
+        console.error(error);
+        return NextResponse.json({error:error.code||error.message},{
+            status:400
+        })
+
+    }
+}
+
+
+//Now check is user have already registered a store if yes then send them status of store 
+
+export async function GET(request){
+    try {
+    const {userId} =getAuth(request)
+
+    //check is user have already registered a store 
+    const store =await prisma.store.findFirst({
+        where:{userId:userId}
+    })
+
+    //if store is already registered then send status of store
+
+    if(store) {
+        return NextResponse.json({status:store.status})
+    }
+    
+    return NextResponse.json({status:"not registered"})
+
+    } catch(error){
+      console.error(error);
+      return NextResponse.json({error:error.code || error.message},   {status:400})
     }
 }
