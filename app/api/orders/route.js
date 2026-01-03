93e0bb941e4bbc18e9fbad4439ee2f9d52e6c44d
Devansh Pandey <devanshpandeyji4321@gmail.com>
2026-01-03 19:01:45 +0530

    cretaed webhook of stripe

--- app/api/orders/route.js
+++ app/api/orders/route.js
@@ -2,6 +2,7 @@ import prisma from "@/lib/prisma";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import {PaymentMethod} from "@prisma/client"
+import Stripe from "stripe";
 
 
 
@@ -113,6 +114,36 @@ for(const [storeId, sellerItems] of ordersByStore.entries()){
 
     orderIds.push(order.id)
 }
+
+    if (paymentMethod === 'STRIPE') {
+  const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+  const origin = await request.headers.get('origin');
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: [{
+      price_data: {
+        currency: 'usd',
+        product_data: {
+          name: 'Order'
+        },
+        unit_amount: Math.round(fullAmount * 100),
+      },
+      quantity: 1
+    }],
+    expires_at:Math.floor(Date.now() / 1000) + 30 * 60,
+    mode: 'payment',
+    success_url: `<${origin}/loading?nextUrl=orders`,
+    cancel_url: `${origin}/cart`,
+    metadata: {
+      orderIds: orderIds.join(','),
+      userId,
+      appId:'sell_well'
+    }
+  })
+  return NextResponse.json({session})
+}
+
     //clear the cart 
     await prisma.user.update({
         where: {id:userId},
